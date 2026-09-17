8163fb533bcba3082c051268ca1cb26d0f1c5dd4
AKnockOnCode <akshitkoul2005@gmail.com>
2026-09-17 12:50:18 +0530

    ✅ Write a function | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-17

--- hackerrank/medium/write-a-function/solution.py
+++ hackerrank/medium/write-a-function/solution.py
@@ -1,4 +1,8 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range (n):
-        print(i*i)
+def is_leap(year):
+    leap = False
+    
+    if (year%4==0 and (year%100!=0 or year%400==0)):
+        leap = True
+    
+    return leap
+
